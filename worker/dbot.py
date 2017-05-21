98a757ede7a7398733dfc8468307c21c100cd64f
Jorge Matricali <jorgematricali@gmail.com>
2017-05-21 16:46:18 -0300

    + Multithreading worker with random IPv4

--- worker/dbot.py
+++ worker/dbot.py
@@ -1,44 +1,91 @@
 #!/usr/bin/env python
 """
-# Python-DoraBot
+Copyright 2017 Jorge Matricali
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
+
 from __future__ import print_function
 import sys
 import nmap
 import time
+import threading
+import argparse
+import signal
 from elasticsearch import Elasticsearch
 from geoip import geolite2
 from faker import Faker
-import threading
 
-es = Elasticsearch()
+exit_flag = False
+threads = []
+es = Elasticsearch(['elasticsearch'])
 nma = nmap.PortScannerAsync()
 faker = Faker()
+PORT_LIST = """7,9,13,21,22,23,25,26,37,53,79,80,81,82,83,84,85,106,110,111,113,
+119,135,139,143,144,179,389,427,443,444,445,465,990,993,995,1025,1027,1433,2049,
+2121,3128,3306,3389,5009,5432,5631,5666,5800,5900,8000,8008,8009,8080,8081,8443,
+9100,9200,5672,27017,27015,25565"""
+
+
+def teardown(signal=0):
+    print('Stopping threads...')
+    exit_flag = True
+    try:
+        for t in threads:
+            try:
+                t.stop()
+            except Exception as e:
+                print(e)
+    except Exception as e:
+        print(e)
+    sys.exit(signal)
+
+
+def signal_handler(signal, frame):
+    teardown(0)
+
 
 class scanThread (threading.Thread):
     def __init__(self, threadID, name, **kwargs):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        if 'target' in kwargs:
-            self.target = kwargs['target']
-        else:
-            self.target = faker.ipv4()
+        super(scanThread, self).__init__()
+        self._threadID = threadID
+        self._name = name
+        self._running = True
+
     def run(self):
         self.log('Starting thread...')
-        nma = nmap.PortScannerAsync()
-        while True:
+        self._nma = nmap.PortScannerAsync()
+        while self._running:
             try:
                 target = faker.ipv4()
                 self.log('Performing scan on', target)
-                nma.scan(target, '7,9,13,21,22,23,25,26,37,53,79,80,81,82,83,84,85,106,110,111,113,119,135,139,143,144,179,389,427,443,444,445,465,990,993,995,1025,1027,1433,2049,2121,3128,3306,3389,5009,5432,5631,5666,5800,5900,8000,8008,8009,8080,8081,8443,9100,9200,5672,27017,27015,25565', arguments='-A', callback=self.procesar_resultado)
-                while nma.still_scanning():
-                    nma.wait(2)
-            except KeyboardInterrupt:
+                self._nma.scan(
+                    target, PORT_LIST, arguments='-A',
+                    callback=self.procesar_resultado)
+                while self._nma.still_scanning():
+                    self._nma.wait(2)
+            except Exception:
                 self.log('Exiting thread...')
+                self.stop()
+
+    def stop(self):
+        self.log('Stopping...')
+        self._running = False
+
     def procesar_resultado(self, host, scan_result):
         if scan_result['nmap']['scanstats']['uphosts'] == '1':
-            self.log(host, 'UP', scan_result['scan'][host]['hostnames'][0]['name'])
+            self.log(
+                host, 'UP', scan_result['scan'][host]['hostnames'][0]['name'])
             data = scan_result['scan'][host]
             data['last_scan'] = time.time()
             ntcp = []
@@ -63,62 +110,35 @@ class scanThread (threading.Thread):
             res = es.index(index='dora', doc_type='host', id=host, body=data)
         else:
             self.log(host, 'DOWN')
+
     def log(self, *args):
-        print(time.ctime(time.time()), self.name, *args)
+        print(time.ctime(time.time()), self._name, *args)
 
-# def infiniteRandom():
-#     while True:
-#         try:
-#             target = faker.ipv4()
-#             print 'Performing scan on', target
-#             nma.scan(target, '7,9,13,21,22,23,25,26,37,53,79,80,81,82,83,84,85,106,110,111,113,119,135,139,143,144,179,389,427,443,444,445,465,990,993,995,1025,1027,1433,2049,2121,3128,3306,3389,5009,5432,5631,5666,5800,5900,8000,8008,8009,8080,8081,8443,9100,9200,5672,27017,27015,25565', arguments='-A', callback=procesar_resultado)
-#             while nma.still_scanning():
-#                 nma.wait(2)
-#         except KeyboardInterrupt:
-#             print 'Exiting...'
-#             sys.exit()
-
-# try:
-#     target = sys.argv[1]
-# except IndexError:
-#     target = faker.ipv4()
-#     infiniteRandom()
-#     sys.exit()
-
-# nma.scan(target, '7,9,13,21,22,23,25,26,37,53,79,80,81,82,83,84,85,106,110,111,113,119,135,139,143,144,179,389,427,443,444,445,465,990,993,995,1025,1027,1433,2049,2121,3128,3306,3389,5009,5432,5631,5666,5800,5900,8000,8008,8009,8080,8081,8443,9100,9200,5672,27017,27015,25565', arguments='-A', callback=procesar_resultado)
-# while nma.still_scanning():
-#     print("Aguantiiando >>>")
-#     nma.wait(2)
 
 if __name__ == '__main__':
-    thread1 = scanThread(1, 'Thread-1')
-    thread2 = scanThread(2, 'Thread-2')
-    thread3 = scanThread(3, 'Thread-3')
-    thread4 = scanThread(4, 'Thread-4')
-    thread5 = scanThread(5, 'Thread-5')
-    thread6 = scanThread(6, 'Thread-6')
-    thread7 = scanThread(7, 'Thread-7')
-    thread8 = scanThread(8, 'Thread-8')
+    signal.signal(signal.SIGINT, signal_handler)
+    parser = argparse.ArgumentParser(description='Dora Scanner Worker')
 
-    # Start new Threads
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    thread4.start()
-    thread5.start()
-    thread6.start()
-    thread7.start()
-    thread8.start()
+    parser.add_argument('-t', '--threads',
+                        dest='numThreads', action='store', type=int,
+                        default=1, help='Total number of threads to use')
 
-    while True:
-        try:
-            pass
-        except KeyboardInterrupt:
-            print('Killing program...')
-            for t in threading.enumerate():
-                try:
-                    t.stop()
-                except:
-                    pass
+    args = parser.parse_args()
+
+    try:
+        numThreads = int(args.numThreads)
+    except NameError:
+        numThreads = 1
+
+    print('Starting %d threads...' % numThreads)
+
+    threads = [None]*numThreads
+    for nt in range(numThreads):
+        threads[nt] = scanThread(nt+1, 'Thread-%d' % (nt+1))
+        threads[nt].daemon = True
+        threads[nt].start()
+
+    while not exit_flag:
+        pass
 
     print('Exiting Main Thread')
--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Starting Dora Worker in 60 seconds..."
+sleep 60
 echo "Running Dora Worker"
-python2 /dbot.py
+python2 /dbot.py -t4
