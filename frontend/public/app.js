f7e10f4160230cf04642c6b4ae7e8371c02f0c5c
Jorge Matricali <jorgematricali@gmail.com>
2017-05-22 03:49:32 -0300

    + Frontend update + Geolocated

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,20 +1,47 @@
-'use strict';
+/**
+Copyright 2017 Jorge Matricali
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+'use strict'
 
 // Declare app level module which depends on views, and components
 angular.module('doraFrontend', [
   'ngRoute',
   'ngAnimate',
+  'ui.bootstrap',
+  'uiGmapgoogle-maps',
   'doraFrontend.view1',
   'doraFrontend.view2',
   'doraFrontend.search',
   'doraFrontend.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}])
-.filter('urlencode', function() {
-  return function(input) {
-    return window.encodeURIComponent(input);
+])
+.config([
+  '$locationProvider',
+  '$routeProvider',
+  'uiGmapGoogleMapApiProvider',
+  function ($locationProvider, $routeProvider, GoogleMapApiProviders) {
+    $locationProvider.hashPrefix('!')
+    $routeProvider.otherwise({redirectTo: '/view1'})
+    GoogleMapApiProviders.configure({
+      v: '3.20',
+      libraries: 'weather,geometry,visualization'
+    })
   }
-});
+])
+.filter('urlencode', function () {
+  return function (input) {
+    return window.encodeURIComponent(input)
+  }
+})
--- frontend/public/search/search.js
+++ frontend/public/search/search.js
@@ -1,62 +1,112 @@
-'use strict';
+'use strict'
 
 angular.module('doraFrontend.search', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config([
+  '$routeProvider',
+  function ($routeProvider, GoogleMapApiProviders) {
     $routeProvider.when('/host/:query*', {
-        templateUrl: 'search/host.html',
-        controller: 'SearchByHostCtrl'
-    });
+      templateUrl: 'search/host.html',
+      controller: 'SearchByHostCtrl'
+    })
     $routeProvider.when('/search/port/:proto/:port*', {
-        templateUrl: 'search/host.html',
-        controller: 'SearchByPortCtrl'
-    });
+      templateUrl: 'search/host.html',
+      controller: 'SearchByPortCtrl'
+    })
     $routeProvider.when('/search/:query*', {
-        templateUrl: 'search/search.html',
-        controller: 'SearchByKeywordCtrl'
-    });
-}])
-
-.controller('SearchByHostCtrl', ['$scope', '$route', '$routeParams', '$http',
-function($scope, $route, $routeParams, $http) {
-    $scope.keyword = $routeParams.query;
-
-    $http.get('http://localhost:9200/dbot/host/_search?q=addresses.ipv4:' + $routeParams.query + '&size=50').then(function(response) {
-        console.log('API /dbot/host/_search?q=addresses.ipv4', response);
-        $scope.currentServices = response.data.hits.hits;
-    });
-
-    $http.get('https://ipinfo.io/' + $routeParams.query).then(function(response) {
-        $scope.hostGeodata = response.data
-    });
-
-    $http.get('https://www.googleapis.com/pagespeedonline/v1/runPagespeed?screenshot=true&url=http://' + $routeParams.query).then(function(response) {
-        var base = response.data.screenshot.data;
-        $scope.base64screenshot = 'data:image/jpg;base64,' + base.replace(/_/g, '/').replace(/-/g,'+');
-    });
-}
+      templateUrl: 'search/search.html',
+      controller: 'SearchByKeywordCtrl'
+    })
+  }
 ])
 
-.controller('SearchByKeywordCtrl', ['$scope', '$route', '$routeParams', '$http',
-function($scope, $route, $routeParams, $http) {
-    $scope.keyword = $routeParams.query;
-    $scope.currentServices = {};
+.controller('SearchByHostCtrl', ['$scope', '$route', '$routeParams', '$http',
+  function ($scope, $route, $routeParams, $http) {
+    $scope.keyword = $routeParams.query
 
-    $http.get('http://localhost:9200/dbot/host/_search?q=' + $routeParams.query + '&size=50').then(function(response) {
-        console.log('API /dbot/host/_search', response);
-        $scope.currentServices = response.data.hits.hits;
-    });
-}
+    $http.get('http://localhost:9200/dora/host/_search?q=addresses.ipv4:' + $routeParams.query + '&size=50').then(function (response) {
+      console.log('API /dora/host/_search?q=addresses.ipv4', response)
+      $scope.currentServices = response.data.hits.hits
+    })
+
+    $http.get('https://ipinfo.io/' + $routeParams.query).then(function (response) {
+      $scope.hostGeodata = response.data
+    })
+
+    $http.get('https://www.googleapis.com/pagespeedonline/v1/runPagespeed?screenshot=true&url=http://' + $routeParams.query).then(function (response) {
+      var base = response.data.screenshot.data
+      $scope.base64screenshot = 'data:image/jpg;base64,' + base.replace(/_/g, '/').replace(/-/g, '+')
+    })
+  }
+])
+
+.controller('SearchByKeywordCtrl', ['$scope', '$route', '$routeParams', '$http', '$location', 'uiGmapGoogleMapApi',
+  function ($scope, $route, $routeParams, $http, $location, uiGmapGoogleMapApi) {
+    $scope.keyword = $routeParams.query
+    $scope.currentServices = {}
+    $scope.go = function (path) {
+      $location.path(path)
+    }
+
+    $http.get('http://localhost:9200/dora/host/_search?q=' + $routeParams.query + '&size=50').then(function (response) {
+      console.log('API /dora/host/_search', response)
+      $scope.currentServices = response.data.hits.hits
+      $scope.totalFound = response.data.hits.total
+
+      $scope.mapMarkers = []
+
+      angular.forEach($scope.currentServices, function (value, key) {
+        try {
+          $scope.mapMarkers.push({
+            position: {
+              latitude: value._source.geo.location.latitude,
+              longitude: value._source.geo.location.longitude
+            },
+            title: value._source.addresses.ipv4,
+            icon: 'assets/images/icon-server-small.png'
+          })
+        } catch (e) {
+
+        }
+      })
+
+      uiGmapGoogleMapApi.then(function (maps) {
+            // Configuration needed to display the road-map with traffic
+            // Displaying Ile-de-france (Paris neighbourhood)
+        maps.visualRefresh = true
+
+        $scope.map = {
+          center: {
+            latitude: $scope.currentServices[0]._source.geo.location.latitude,
+            longitude: $scope.currentServices[0]._source.geo.location.longitude
+          },
+          zoom: 8,
+          options: {
+            mapTypeId: google.maps.MapTypeId.ROADMAP, // This is an example of a variable that cannot be placed outside of uiGmapGooogleMapApi without forcing of calling ( like ugly people ) the google.map helper outside of the function
+            streetViewControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            rotateControl: false,
+            zoomControl: false
+          },
+          showTraficLayer: true
+        }
+
+        console.log($scope.map)
+        $scope.isOffline = false
+      })
+    })
+  }
 ])
 
 .controller('SearchByPortCtrl', ['$scope', '$route', '$routeParams', '$http',
-function($scope, $route, $routeParams, $http) {
-    $scope.keyword = $routeParams.proto + ' ' + $routeParams.port;
-    $scope.currentServices = {};
+  function ($scope, $route, $routeParams, $http) {
+    $scope.keyword = $routeParams.proto + ' ' + $routeParams.port
+    $scope.currentServices = {}
 
-    $http.get('http://localhost:9200/dbot/host/_search?q=services.proto:' + $routeParams.proto + ';services.port:' + $routeParams.port + '&size=50').then(function(response) {
-        console.log('API /dbot/host/_search', response);
-        $scope.currentServices = response.data.hits.hits;
-    });
-}
-]);
+    $http.get('http://localhost:9200/dora/host/_search?q=services.proto:' + $routeParams.proto + ';services.port:' + $routeParams.port + '&size=50').then(function (response) {
+      console.log('API /dora/host/_search', response)
+      $scope.currentServices = response.data.hits.hits
+    })
+  }
+])
